6529e94534150b4266eb76f94fd570277949904a
pablo.ruhl <pablo.ruhl@policiadelaciudad.gob.ar>
2021-06-03 15:35:02 -0300

    Add Expire Time Config

---
+++ vendor/composer/installed.php
@@ -0,0 +1,650 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '94eaa39ea9d505058c3cb77fd7d693a29adef0b0',
+    'name' => 'sirpaul/laravel_ssomanager',
+  ),
+  'versions' => 
+  array (
+    'brick/math' => 
+    array (
+      'pretty_version' => '0.9.2',
+      'version' => '0.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dff976c2f3487d42c1db75a3b180e2b9f0e72ce0',
+    ),
+    'fgrosse/phpasn1' => 
+    array (
+      'pretty_version' => 'v2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '20299033c35f4300eb656e7e8e88cf52d1d6694e',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '7.4.x-dev',
+      ),
+      'reference' => 'de6f1e58e735754b888649495ed4cb9ae3b19589',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.4.x-dev',
+      ),
+      'reference' => '8e7d04f1f6450fef59366c399cfad4b9383aa30d',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '2.0.x-dev',
+      ),
+      'reference' => '148492f9e2e1524317a8aa60875f5b59517a9374',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.1.x-dev',
+      'version' => '1.1.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8622567409010282b7aeebe4bb841fe98b58dcaf',
+    ),
+    'psr/container-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/event-dispatcher' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => 'aa4f89e91c423b516ff226c50dc83f824011c253',
+    ),
+    'psr/event-dispatcher-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => '22b2ef5687f43679481615605d7a15c557ce85b1',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => '36fa03d50ff82abcae81860bdaf4ed9a1510c7cd',
+    ),
+    'psr/http-factory-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => 'efd67d1dc14a7ef4fc4e518e7dee91c271d524e4',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.1.x-dev',
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'sirpaul/laravel_ssomanager' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '94eaa39ea9d505058c3cb77fd7d693a29adef0b0',
+    ),
+    'spomky-labs/aes-key-wrap' => 
+    array (
+      'pretty_version' => 'v6.0.0',
+      'version' => '6.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '97388255a37ad6fb1ed332d07e61fa2b7bb62e0d',
+    ),
+    'spomky-labs/base64url' => 
+    array (
+      'pretty_version' => 'v2.x-dev',
+      'version' => '2.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '7752ce931ec285da4ed1f4c5aa27e45e097be61d',
+    ),
+    'symfony/config' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db7f82ee863ec4ebaed87fb2b036a76510259da9',
+    ),
+    'symfony/console' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a41315acd97c15f296092d9fde11a30103ae6c26',
+    ),
+    'symfony/dependency-injection' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2348d4ac592ca74e49d51bf7ccf56bc22b3108b3',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.4.x-dev',
+      ),
+      'reference' => '5f38c8804a9e97d23e0c8d63341088cd8a22d627',
+    ),
+    'symfony/error-handler' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f5ae1a4298b754594c66535bbce0f0d6e06c008d',
+    ),
+    'symfony/event-dispatcher' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aecf90ffee8f1d2a2c169ec51cd1177027fc3ace',
+    ),
+    'symfony/event-dispatcher-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.4.x-dev',
+      ),
+      'reference' => '69fee1ad2332a7cbab3aca13591953da9cdb7a11',
+    ),
+    'symfony/event-dispatcher-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '2.0',
+      ),
+    ),
+    'symfony/filesystem' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '348116319d7fb7d1faa781d26a48922428013eb2',
+    ),
+    'symfony/http-client-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.4.x-dev',
+      ),
+      'reference' => '7e82f6084d7cae521a75ef2cb5c9457bbda785f4',
+    ),
+    'symfony/http-foundation' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd776c3bd60a7a529175b3c4b282f1fecdf6d7b9f',
+    ),
+    'symfony/http-kernel' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6486ca4f71214eb04d0009efb19aab44b488b6ef',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.23.x-dev',
+      ),
+      'reference' => '46cd95797e9df938fdd2b03693b5fca5e64b01ce',
+    ),
+    'symfony/polyfill-intl-grapheme' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.23.x-dev',
+      ),
+      'reference' => '16880ba9c5ebe3642d1995ab866db29270b36535',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.23.x-dev',
+      ),
+      'reference' => '8590a5f561694770bdcd3f9b5c69dde6945028e8',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.23.x-dev',
+      ),
+      'reference' => '9174a3d80210dca8daa7f31fec659150bbeabfc6',
+    ),
+    'symfony/polyfill-php73' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.23.x-dev',
+      ),
+      'reference' => 'fba8933c384d6476ab14fb7b8526e5287ca7e010',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.23.x-dev',
+      ),
+      'reference' => 'eca0bf41ed421bed1b57c4958bab16aa86b757d0',
+    ),
+    'symfony/polyfill-php81' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.23.x-dev',
+      ),
+      'reference' => 'e66119f3de95efc359483f810c4c3e6436279436',
+    ),
+    'symfony/service-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.4.x-dev',
+      ),
+      'reference' => 'f040a30e04b57fbcc9c6cbcf4dbaa96bd318b9bb',
+    ),
+    'symfony/service-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0|2.0',
+      ),
+    ),
+    'symfony/string' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '07417941ec86fa429b4b108e1217a656816483d5',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => '5.4.x-dev',
+      'version' => '5.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '173cf39010292d1de114f3a086e50fd27dc9d0d6',
+    ),
+    'thecodingmachine/safe' => 
+    array (
+      'pretty_version' => 'v1.3.3',
+      'version' => '1.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a8ab0876305a4cdaef31b2350fcb9811b5608dbc',
+    ),
+    'web-token/encryption-pack' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-bundle' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-checker' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-console' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-core' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-easy' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-aescbc' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-aesgcm' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-aesgcmkw' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-aeskw' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-dir' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-ecdh-es' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-experimental' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-pbes2' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-encryption-algorithm-rsa' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-framework' => 
+    array (
+      'pretty_version' => 'v2.2.x-dev',
+      'version' => '2.2.9999999.9999999-dev',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '5ec0bae9696850b449925308ce1b498dfe76ce37',
+    ),
+    'web-token/jwt-key-mgmt' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-nested-token' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-signature' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-signature-algorithm-ecdsa' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-signature-algorithm-eddsa' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-signature-algorithm-experimental' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-signature-algorithm-hmac' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-signature-algorithm-none' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-signature-algorithm-rsa' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/jwt-util-ecc' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+    'web-token/signature-pack' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.2.x-dev',
+        1 => '9999999-dev',
+      ),
+    ),
+  ),
+);
